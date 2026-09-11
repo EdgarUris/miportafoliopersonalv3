e307e97498c133ffa32b26eb1c48e81a2162b23f
Edgar Urias <edgar.urias261147@potros.itson.edu.mx>
2026-09-11 12:04:23 -0700

    subir nuevo portafolio personal
    nueva version

---
+++ src/main/java/com/example/miportafoliopersonalv3/Miportafoliopersonalv3Application.java
@@ -0,0 +1,13 @@
+package com.example.miportafoliopersonalv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miportafoliopersonalv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Miportafoliopersonalv3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miportafoliopersonalv3/Miportafoliopersonalv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miportafoliopersonalv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Miportafoliopersonalv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
